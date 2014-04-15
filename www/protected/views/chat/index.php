73846431a52f31b49f5286343cff50a22fa3de2e
ulasevichv <ulasevichv@teqniksoft.com>
2014-04-15 20:12:22 +0300

    Chat design

--- www/protected/views/chat/index.php
+++ www/protected/views/chat/index.php
@@ -12,14 +12,22 @@ Yii::app()->clientScript->registerCssFile('http://fonts.googleapis.com/css?famil
 <div class="chatRoot">
 	<div class="header">
 	</div>
-	<div id="groups">
-		Groups
-	</div>
-	<div id="users">
-		Users
-	</div>
-	<div id="messages">
-		Messages
+	<div class="sections">
+		<div id="groups">
+			Groups
+		</div>
+		<div id="users">
+			Users
+		</div>
+		<div id="chat">
+			<div id="messages"></div>
+			<div id="sending">
+				<div class="controls">
+					<?php echo CHtml::textArea(null, '', array('id' => 'inputMessage')); ?>
+					<?php echo CHtml::htmlButton(Yii::t('general', 'Send'), array('id' => 'btnSend', 'class' => 'btn btn-primary')); ?>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 
@@ -29,10 +37,20 @@ Yii::app()->clientScript->registerScript(uniqid(), "
 	var groups = [
 		{ id : 1, name : 'Marketing', numUsersOnline : 3 },
 		{ id : 2, name : 'Sellers', numUsersOnline : 0 },
-		{ id : 3, name : 'Teqniksoft', numUsersOnline: 1 }
+		{ id : 3, name : 'Teqniksoft', numUsersOnline: 1 },
+		{ id : 4, name : 'Teqspring', numUsersOnline: 0 },
+		{ id : 5, name : 'Sysadmins', numUsersOnline: 6 }
+	];
+	
+	var messages = [
+		{ time : '2014-04-15 16:25:02', from : 'Vladimir Putin', text : 'Hello' },
+		{ time : '2014-04-15 16:27:14', from : 'Vladimir Putin', text : 'Hey, Victor, are you there?' },
+		{ time : '2014-04-15 16:27:25', from : 'Victor Ulasevich', text : 'Yeah, how are you? How\'s the situation with Crimea?' },
+		{ time : '2014-04-15 16:27:44', from : 'Vladimir Putin', text : 'All good, all good...' }
 	];
 	
 	var openedGroup = null;
+	var sendingDivSize = null;
 	
 	function getGroupById(id)
 	{
@@ -60,6 +78,30 @@ Yii::app()->clientScript->registerScript(uniqid(), "
 		}
 	}
 	
+	function getSendingDivSize()
+	{
+		var jSending = $('#sending');
+		
+		return { x : jSending.width(), y : jSending.height() };
+	}
+	
+	function onWindowResize(initialCall)
+	{
+		initialCall = (typeof(initialCall) != 'undefined' ? initialCall : false);
+		
+		var newSendingDivSize = getSendingDivSize();
+		
+		if (newSendingDivSize.x != sendingDivSize.x || initialCall)
+		{
+			var jInputMessage = $('#inputMessage');
+			var jBtnSend = $('#btnSend');
+			
+			jInputMessage.css('width', (newSendingDivSize.x - parseInt(jBtnSend.width()) - 35) + 'px');
+		}
+		
+		sendingDivSize = newSendingDivSize;
+	}
+	
 ", CClientScript::POS_HEAD);
 
 Yii::app()->clientScript->registerScript(uniqid(), "
@@ -78,6 +120,31 @@ Yii::app()->clientScript->registerScript(uniqid(), "
 	
 	$('#groups').html(feed.join(''));
 	
+	feed = [];
+	
+	messages.forEach(function(message, i)
+	{
+		var blockType = (message.from == 'Victor Ulasevich' ? 'outgoing' : 'incoming');
+		
+		feed.push('<div class=\"message ' + blockType + '\">');
+		feed.push(	'<div class=\"from\">');
+//		feed.push(		'<div class=\"txt\">');
+		feed.push(			message.from);
+//		feed.push(		'</div>');
+		feed.push(	'</div>');
+		feed.push(	'<div class=\"text\">');
+		feed.push(		message.text);
+		feed.push(	'</div>');
+		feed.push(	'<div class=\"time\">');
+		feed.push(		message.time);
+		feed.push(	'</div>');
+		feed.push('</div>');
+	});
+	
+	$('#messages').html(feed.join(''));
+	
+	sendingDivSize = getSendingDivSize();
+	onWindowResize(true);
 	updateChat();
 	
 	$('#groups').on('click', '> .group', function()
@@ -99,6 +166,11 @@ Yii::app()->clientScript->registerScript(uniqid(), "
 		}
 		
 		updateChat();
+	});
+	
+	$(window).resize(function()
+	{
+		onWindowResize();
 	});
 	
 ", CClientScript::POS_READY);
