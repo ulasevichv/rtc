7cd89c0b87c5e35a5ac4e3ba9bd056fd24ca80f7
dovgailo_v <vadim.dovgailo@teqspring.net>
2014-05-15 13:39:14 +0300

    remove toJID from chat history

--- www/protected/views/chat/_chat_history.php
+++ www/protected/views/chat/_chat_history.php
@@ -9,7 +9,6 @@ $this->widget('zii.widgets.grid.CGridView', array(
         'conversationID',
         'sentDate'=>array('name'=>'Date', 'value'=>'Yii::app()->dateFormatter->format("MMMM dd, y hh:mm:ss a", intval($data["sentDate"]/1000))'),
         'fromJID',
-        'toJID',
         'body',
     ),
     'itemsCssClass' => 'table table-striped',
