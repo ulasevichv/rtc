7c3a650051593b6a539197c83690ed3be65aaa97
ulasevichv <ulasevichv@teqniksoft.com>
2014-05-27 17:21:13 +0300

    reg fix

--- www/protected/controllers/UserController.php
+++ www/protected/controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 				'backColor' => 0xffffff,
 				'foreColor' => 0x009900,
 				'offset' => 1,
-				'testLimit' => 3,
+				'testLimit' => 10,
 			),
 		);
 	}
