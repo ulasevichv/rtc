47ba26ce781a38ac63c056971f6b7bffb05777b4
ulasevichv <ulasevichv@teqniksoft.com>
2014-06-12 19:13:56 +0300

    Screen sharing testing

--- www/protected/views/screenSharing/index.php
+++ www/protected/views/screenSharing/index.php
@@ -8,28 +8,49 @@ Yii::app()->clientScript->registerScript(uniqid('chat_js'), "
 	var streamingStarted = false;
 	var mainStream = null;
 	
-	function validateRequirements()
+	function validateRequirementsAndGetUserMedia()
 	{
+		// Browser versions examples:
+		// Firefox: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0
+		// Chrome:  Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36
+		
 		if (!location.protocol.match('https')) return 'You need to run this application from https';
 		
-		if (!(navigator.userAgent.match('Chrome') && parseInt(navigator.userAgent.match(/Chrome\/(.*) /)[1]) >= 26)) return 'You need Chrome 26+ to run this application';
-		
-		if (!navigator.getUserMedia && !navigator.webkitGetUserMedia) return 'navigator.getUserMedia is not supported by your browser';
+		if (navigator.userAgent.match('Chrome'))
+		{
+			var browserMajorVersion = parseInt(navigator.userAgent.match(/ Chrome\/(.*)/)[1]);
+			
+			if (browserMajorVersion < 26) return 'You need Chrome 26+ to run this application';
+			
+			navigator.getUserMedia = navigator.webkitGetUserMedia;
+		}
+		else if (navigator.userAgent.match('Firefox'))
+		{
+			var browserMajorVersion = parseInt(navigator.userAgent.match(/ Firefox\/(.*)/)[1]);
+			
+			if (browserMajorVersion < 24) return 'You need Firefox 24+ to run this application';
+			
+			navigator.getUserMedia = navigator.mozGetUserMedia;
+			
+			return 'You need Chrome to run this application'; // Doesn't really work in Firefox.
+		}
+		else
+		{
+			return 'This application needs to be executed in Firefox or Chrome';
+		}
 		
 		return '';
 	}
 	
 	function getScreen()
 	{
-		var error = validateRequirements();
+		var error = validateRequirementsAndGetUserMedia();
 		
 		if (error != '')
 		{
 			alert(error);
 			return;
 		}
-		
-		navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.getUserMedia;
 		
 		navigator.getUserMedia(
 			{
@@ -65,20 +86,39 @@ Yii::app()->clientScript->registerScript(uniqid('chat_js'), "
 		
 		videoContainer.src = window.URL.createObjectURL(stream);
 		videoContainer.autoplay = true;
+		
+		//
+		
+		var peerConnection = new webkitRTCPeerConnection(
+			{ 'iceServers' : [{ 'url' : 'stun:stun.l.google.com:19302' }] }
+		);
+		
+		peerConnection.onicecandidate = onIceCandidate;
+		peerConnection.onaddstream = onAddStream;
+		peerConnection.addStream(stream);
+		
+		console.log(peerConnection);
 	}
 	
 	function onError(error)
 	{
-		if (error.code == error.PERMISSION_DENIED)
+		if (typeof(error.code) != 'undefined' && error.code == error.PERMISSION_DENIED)
 		{
-			alert('PERMISSION_DENIED. Are you no SSL? Have you enabled the --enable-usermedia-screen-capture flag?');
+			alert('PERMISSION_DENIED. Have you enabled the --enable-usermedia-screen-capture flag?');
+		}
+		
+//		alert(JSON.stringify(e, null, '\t'));
+		
+		if (typeof(error.message) != 'undefined')
+		{
+			alert('An error occurred: ' + error.message);
 		}
 		else
 		{
-			console.log('An error occurred: ' + error.message);
-			console.log(error);
-			return;
+			alert('An error occurred: ' + error);
 		}
+		
+		return;
 	}
 	
 	function onEnded()
@@ -89,11 +129,21 @@ Yii::app()->clientScript->registerScript(uniqid('chat_js'), "
 	function endStreaming()
 	{
 		streamingStarted = false;
-		mainStream.stop();
+		if (typeof(mainStream.stop) != 'undefined') mainStream.stop();
 		mainStream = null;
 		
 		var jBtn = $('#btnStartScreenSharing');
 		jBtn.html('Start');
+	}
+	
+	function onIceCandidate()
+	{
+		
+	}
+	
+	function onAddStream()
+	{
+		
 	}
 	
 ", CClientScript::POS_HEAD);
