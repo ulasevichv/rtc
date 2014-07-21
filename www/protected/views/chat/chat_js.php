3c02c36f1a36e575172bc43e41bc5ef09aeb726f
dovgailo_v <vadim.dovgailo@teqspring.net>
2014-07-21 16:07:51 +0300

    config fix

--- www/protected/views/chat/chat_js.php
+++ www/protected/views/chat/chat_js.php
@@ -7,7 +7,7 @@ Yii::app()->clientScript->registerScript(uniqid('chat_js'), "
 		boshAddress : '".$boshAddress."',
 		currentUser : new InternalChatUser(
 			'',
-			'".$xmppUserName."' + '@192.237.219.76',
+			'".$xmppUserName."' + '@' + '".$xmppAddress."',
 			'".$xmppUserName."',
 			'".Yii::app()->user->fullName."',
 			'".$xmppUserPassword."'
